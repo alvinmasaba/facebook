a326c953b385c6d9b40a773b111863206b5f5ce9
alvinmasaba <alvinmasaba@gmail.com>
2022-12-29 20:50:10 -0500

    Generates posts and comments models

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   has_many :sent_requests, class_name: 'FriendRequest', foreign_key: 'sender_id'
   has_many :received_requests, class_name: 'FriendRequest', foreign_key: 'recipient_id'
 
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, through: :posts, foreign_key: 'commenter_id', dependent: :destroy
+
   # Incoming friends are users who have sent the user a request
   has_many :incoming_friends, through: :received_requests, source: 'sender'
 
