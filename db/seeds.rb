b4bd371034d99f410647242463ff866763ecd46b
alvinmasaba <alvinmasaba@gmail.com>
2023-01-06 18:16:06 -0500

    Creates seedfile for users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding database with users..."
+
+User.delete_all
+
+USERS = ["Alvin Masaba", "Francis Masaba", "Stephanie Masaba",
+         "Trevor Masaba", "Cassandra Marambio", "Nick Kerr", "Steve Nash",
+         "LeBron James", "Pascal Siakam", "Chat GPT3"]
+
+USERS.each do |u|
+  name = u.split
+  UserManager::User.create(name: name[0], surname: name[1], email: "#{name[0].downcase}@gmail.com", password: 'masaba', password_confirmation: 'masaba')
+end
