1cd345e354e48ef0bb6a23072338e84d19474127
alvinmasaba <alvinmasaba@gmail.com>
2023-01-19 12:56:41 -0500

    Adds update method to posts controller to permit nested comment creation. Fixes bug preventing this.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :update ]
+
   def index
     @post = Post.new
     @posts = current_user.recent_friends_posts
-
-    @posts.each { |post| post.comments.build }
   end
 
   def create
@@ -12,9 +12,26 @@ class PostsController < ApplicationController
     ActionCable.server.broadcast('post', @post.as_json(include: :author))
   end
 
+  def update
+    respond_to do |format|
+      if @post.update!(post_params)
+        format.html { redirect_to root_path }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :index, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
   def post_params
-    params.require(:post).permit(:body, comments_attributes: [:id, :body, :_destroy])
+    params.require(:post).permit(:body, comments_attributes: [:id, :commenter_id, :body, :_destroy])
   end
 end
