910d6cea07e92245561b2f6b48008b07a2d43815
alvinmasaba <alvinmasaba@gmail.com>
2023-01-23 15:13:12 -0500

    Defines likers method on post model utilizing user_likers association to display names of likers next to number of likes on posts.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :commenter, class_name: 'User'
   has_many :likes
+  has_many :user_likers, through: :likes, source: "commenter_id"
 
   accepts_nested_attributes_for :likes, allow_destroy: true, reject_if: :all_blank
   
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,16 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :user_likers, through: :likes, source: :user
   
   accepts_nested_attributes_for :comments, allow_destroy: true, reject_if: :all_blank
   accepts_nested_attributes_for :likes, allow_destroy: true, reject_if: :all_blank
 
   validates_presence_of :body
+
+  def likers
+    self.user_likers.limit(4)
+                    .map { |user| user.full_name }
+                    .join(", ")
+  end
 end
