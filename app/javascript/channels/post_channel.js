82711de8f1e8b5d96765d33c690b714732a2da3e
alvinmasaba <alvinmasaba@gmail.com>
2023-03-27 14:52:46 -0400

    Updated received function in post_channel to simply reload the page after a new post or comment or like, for simplicity, and to remove the superfluous turbo stream functionality of inserting a dummy comment block. Looked nice but wasn't a functional post.

--- app/javascript/channels/post_channel.js
+++ app/javascript/channels/post_channel.js
@@ -9,28 +9,8 @@ const postChannel = consumer.subscriptions.create("PostChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received(data) {
+  received() {
     // Called when there's incoming data on the websocket for this channel
-    const postDisplay = document.querySelector('#post-display')
-    postDisplay.insertAdjacentHTML('afterbegin', this.template(data))
-  },
-
-  template(data) {
-    return `<div class="card mb-5">
-              <div class="card-body">
-                <div class="d-flex flex-start align-items-center">
-                  <img class="rounded-circle shadow-1-strong me-3"
-                    src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(19).webp" alt="avatar" width="60"
-                    height="60" />
-                  <div>
-                    <h6 class="fw-bold text-primary mb-1">${data.author.first_name}</h6>
-                  </div>
-                </div>
-            
-                <p class="mt-3 mb-4 pb-2">
-                  ${data.body}
-                </p>
-              </div>
-            </div>`
+    return location.reload()
   }
 });
